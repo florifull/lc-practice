8bace92309f27c85c2f27c66982ebc790ba408d6
florifull <93046441+florifull@users.noreply.github.com>
2025-06-28 20:35:34 -0400

    Time: 3 ms (81.14%) | Memory: 18.4 MB (90.21%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,7 +1,17 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # take advantage of the fact sorted arr..
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            ourSum = numbers[l] + numbers[r]
+            if ourSum == target: return [l+1, r+1]
+            elif ourSum > target: r -= 1
+            else: l += 1
+        """
+        T: O(n), S: O(n)
         nums = {}
         for i in range(len(numbers)):
             if target - numbers[i] in nums:
                 return [1 + nums[target-numbers[i]], i + 1]
             nums[numbers[i]] = i
+        """
