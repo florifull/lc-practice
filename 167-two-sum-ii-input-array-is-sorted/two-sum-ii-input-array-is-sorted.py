76589c4dfc48289000991ea3f3480295779f274e
florifull <93046441+florifull@users.noreply.github.com>
2025-06-28 20:35:53 -0400

    Time: 3 ms (81.14%) | Memory: 18.7 MB (32.86%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # take advantage of the fact sorted arr..
+        # take advantage of the fact sorted arr.. (T: O(n), S: O(1))
         l, r = 0, len(numbers) - 1
         while l < r:
             ourSum = numbers[l] + numbers[r]
