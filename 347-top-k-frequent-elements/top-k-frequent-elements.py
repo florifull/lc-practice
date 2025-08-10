4acd22fd73976afcd7620aef26fe280fc7378209
florifull <93046441+florifull@users.noreply.github.com>
2025-08-10 19:42:03 -0400

    Time: 11 ms (23.31%) | Memory: 24.4 MB (7.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # count = {1:3, 2:2, 3:1} (number:it's occurences)
-        # bucket = [[x, x, x, x, x, [x,y], [x]]
-        count = collections.Counter(nums)
-        bucket = [[] for _ in range(len(nums)+1)]
-        # fill our bucket
-        for num, occ in count.items():
-            bucket[occ].append(num)
-        # return k most freq elements
-        print(bucket)
-        ans = []
-        for i in range(len(bucket)-1, -1, -1):
-            if bucket[i]:
-                ans.extend(bucket[i][:])
-                freq = len(bucket[i])
-                k -= freq
-                if k <= 0: break
-        return ans
+        counts = collections.Counter(nums)
+        ans = [[] for i in range(len(nums) + 1)]
+        
+        for num, occ in counts.items():
+            ans[occ].append(num)
+        ret = []
+        for i in range(len(ans)-1, -1, -1):
+            if ans[i]:
+                ret.extend(ans[i][:])
+                if len(ret) == k:
+                    return ret
