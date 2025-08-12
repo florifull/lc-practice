b8d64220b322b1ff2e5d9240c16870e52f7efc81
florifull <93046441+florifull@users.noreply.github.com>
2025-08-11 23:21:57 -0400

    Time: 20 ms (74.02%) | Memory: 23.1 MB (98.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,11 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix, postfix = [1] * len(nums), [1] * len(nums)
-        for i in range(1, len(prefix)):
-            prefix[i] = nums[i-1] * prefix[i-1]
-        for i in range(len(prefix)-2, -1, -1):
-            postfix[i] = nums[i + 1] * postfix[i+1]
-        r_arr = [1] * len(nums)
-        for i in range(len(r_arr)):
-            r_arr[i] = prefix[i] * postfix[i]
-        return r_arr
+        product = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            product[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            product[i] *= postfix
+            postfix *= nums[i]
+        return product
+        # prefix, postfix = [1] * len(nums), [1] * len(nums)
+        # for i in range(1, len(prefix)):
+        #     prefix[i] = nums[i-1] * prefix[i-1]
+        # for i in range(len(postfix)-2, -1, -1):
+        #     postfix[i] = nums[i + 1] * postfix[i+1]
+        # r_arr = [1] * len(nums)
+        # for i in range(len(r_arr)):
+        #     r_arr[i] = prefix[i] * postfix[i]
+        # return r_arr
