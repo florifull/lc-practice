0cc26e0c6a8e08422d24c41fc5e4ce9ba891b901
florifull <93046441+florifull@users.noreply.github.com>
2025-08-12 14:21:57 -0400

    Time: 526 ms (77.56%) | Memory: 20.6 MB (86.47%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,18 +2,21 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
-        for idx, val in enumerate(nums):
-            # found a repeated leftmost value..
-            if idx > 0 and val == nums[idx-1]: continue
-            l, r = idx+1, len(nums) - 1
-            while l < r:
-                ourSum = val + nums[l] + nums[r]
+        l, r = 0, len(nums) - 1
+        while l < r:
+            r = len(nums) - 1
+            m = l + 1
+            while m < r:
+                ourSum = nums[l] + nums[m] + nums[r]
                 if ourSum == 0:
-                    res.append([val, nums[l], nums[r]])
-                    l += 1
-                    while l < r and nums[l] == nums[l-1]: l += 1
-                elif ourSum < 0:
-                    l += 1
-                else:
+                    res.append([nums[l], nums[m], nums[r]])
+                elif ourSum > 0:
                     r -= 1
+                    continue
+                m += 1
+                while m < r and nums[m] == nums[m-1]:
+                    m += 1
+            l += 1
+            while l < r and nums[l] == nums[l - 1]:
+                l += 1
         return res
