7efd177d1e64cd6246ee846748c4ec1db84db289
florifull <93046441+florifull@users.noreply.github.com>
2025-08-13 02:10:17 -0400

    Time: 21 ms (56.29%) | Memory: 18.1 MB (19.51%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,13 +1,18 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1Val, s2Val = [0] * 26, [0] * 26
+        uniCode = [0] * 26
         for c in s1:
-            s1Val[ord(c) - ord('a')] += 1
+            uniCode[ord(c) - ord('a')] += 1
+        tupleUnicode = tuple(uniCode)
+        
         l = 0
+        uniCode = [0] * 26
         for r in range(len(s2)):
             while r - l + 1 > len(s1):
-                s2Val[ord(s2[l]) - ord('a')] -= 1
+                uniCode[ord(s2[l]) - ord('a')] -= 1
                 l += 1
-            s2Val[ord(s2[r]) - ord('a')] += 1
-            if tuple(s2Val) == tuple(s1Val): return True
+            uniCode[ord(s2[r]) - ord('a')] += 1
+            if tuple(uniCode) == tupleUnicode:
+                return True
         return False
+
