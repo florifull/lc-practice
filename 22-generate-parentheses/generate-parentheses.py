e2b8fe09078dd067c9caf49324152a9d443928e9
florifull <93046441+florifull@users.noreply.github.com>
2025-08-18 02:49:54 +0200

    Time: 0 ms (100.00%) | Memory: 18 MB (43.33%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack, res = [], []
+
+        def bt(openers, closers):
+            # if we have open and close parantheses and maxed out..
+            if openers == closers == n:
+                res.append(''.join(stack[:]))
+                return
+            if openers < n:
+                stack.append('(')
+                bt(openers + 1, closers)
+                stack.pop()
+            if closers < openers:
+                stack.append(')')
+                bt(openers, closers + 1)
+                stack.pop()
+        bt(0, 0)
+        return res
