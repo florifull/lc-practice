c3f8ae5273e31e8302ea63f6a5f5087050f2772a
florifull <93046441+florifull@users.noreply.github.com>
2025-08-18 03:06:14 +0200

    Time: 3 ms (29.56%) | Memory: 18.1 MB (43.33%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -11,7 +11,7 @@ class Solution:
                 stack.append('(')
                 bt(openers + 1, closers)
                 stack.pop()
-            if closers < n and closers < openers:
+            if closers < openers:
                 stack.append(')')
                 bt(openers, closers + 1)
                 stack.pop()
