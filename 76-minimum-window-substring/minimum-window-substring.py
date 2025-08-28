f6661716a9a986c4b8ca4c843a482a39d0d23265
florifull <93046441+florifull@users.noreply.github.com>
2025-08-28 12:20:11 -0400

    Time: 63 ms (80.39%) | Memory: 18.3 MB (34.19%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not s or not t: return ''
+        tOccs = collections.Counter(t)
+        sOccs = {}
+        totalS, totalT = 0, len(t)
+        l = 0
+        minL, twoPtrs = float('inf'), [-1, -1]
+
+        for r, sChar in enumerate(s):
+            if sChar in tOccs:
+                sOccs[sChar] = sOccs.get(sChar, 0) + 1
+                if sOccs[sChar] <= tOccs[sChar]:
+                    totalS += 1
+            while totalS == totalT:
+                if r - l + 1 < minL:
+                    minL = r - l + 1
+                    twoPtrs = [l, r]
+                if s[l] in tOccs:
+                    if sOccs[s[l]] == tOccs[s[l]]:
+                        totalS -= 1
+                    sOccs[s[l]] -= 1
+                l += 1
+        return s[twoPtrs[0]:twoPtrs[1]+1] if minL != float('inf') else ''
