df3c618337df49911e48069ad010897bdc0b807e
florifull <93046441+florifull@users.noreply.github.com>
2025-08-29 22:35:52 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (98.79%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,21 +5,18 @@ class Solution:
 
         while l <= r:
             m = (l + r) // 2
-            if nums[m] == target: return m
-            elif nums[l] == target: return l
-            elif nums[r] == target: return r
-
+            if target == nums[m]: return m
             # determine sorted halfs
             if nums[m] <= nums[r]: # right side is sorted
                 # strictly increasing rightside - nums[m] < target < nums[r]
-                if target > nums[m] and target < nums[r]:
+                if target > nums[m] and target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
             elif nums[m] >= nums[l]: # leftside is sorted
                 # strictly increasing leftside - nums[l] < target < nums[m]
-                if target < nums[m] and target > nums[l]:
+                if target < nums[m] and target >= nums[l]:
                     r = m - 1
                 else:
                     l = m + 1
-        return targetIdx       
+        return -1 
