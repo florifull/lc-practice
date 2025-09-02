81cc8180cbdbfc3a082db1a01019926ad9224b24
florifull <93046441+florifull@users.noreply.github.com>
2025-09-01 22:58:31 -0400

    Time: 45 ms (29.32%) | Memory: 18.5 MB (59.86%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -10,21 +10,16 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head: return None
-        originalNodes = collections.defaultdict(list)
+        oldToNew = {}
         ptr = head
-        # traverse og LL, add mapping (node : [newNode, node.next, node.random])
         while ptr:
-            originalNodes[ptr].extend([Node(ptr.val), ptr.next, ptr.random])
+            oldToNew[ptr] = Node(ptr.val)
             ptr = ptr.next
-
-        ogPtr = head
-        while ogPtr:
-            freshNode, nextPtr, randomPtr = originalNodes[ogPtr]
-            # extract new node object for each of the original nodes
-            if nextPtr:
-                freshNode.next = originalNodes[nextPtr][0]
-            if randomPtr:
-                freshNode.random = originalNodes[randomPtr][0]
-            # move on
-            ogPtr = ogPtr.next
-        return originalNodes[head][0]
+        
+        ptr = head
+        while ptr:
+            newNode = oldToNew[ptr]
+            newNode.next = None if not ptr.next else oldToNew[ptr.next]
+            newNode.random = None if not ptr.random else oldToNew[ptr.random]
+            ptr = ptr.next
+        return oldToNew[head]
