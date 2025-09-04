3e95b13afab78eb8511730f4a325d98224a2c255
florifull <93046441+florifull@users.noreply.github.com>
2025-09-03 20:00:25 -0400

    Time: 2 ms (96.77%) | Memory: 20.9 MB (20.63%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -7,7 +7,7 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.diameter = 0
-        
+
         def dfs(node):
             if not node: return 0
 
@@ -15,8 +15,6 @@ class Solution:
             maxRightHeight = dfs(node.right)
             localDiameter = maxLeftHeight + maxRightHeight
             if localDiameter > self.diameter: self.diameter = localDiameter
-            # return max height from said node
-            return max(maxLeftHeight, maxRightHeight) + 1
+            return 1 + max(maxLeftHeight, maxRightHeight)
         dfs(root)
         return self.diameter
-    # T: O(n), S: O(h)
