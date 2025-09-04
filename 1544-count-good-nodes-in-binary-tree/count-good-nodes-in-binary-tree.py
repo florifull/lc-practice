915ca98d6e5c93e6bf8add5eeaeb26a93628e725
florifull <93046441+florifull@users.noreply.github.com>
2025-09-04 19:25:18 -0400

    Time: 128 ms (91.13%) | Memory: 32.3 MB (33.08%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -6,14 +6,26 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        counter = 0
-        q = collections.deque() # hold node : maxPathVal
-        q.append((root, root.val))
-        while q:
-            node, maxPathVal = q.popleft()
-            if node.val >= maxPathVal: counter += 1
-            if node.left:
-                q.append((node.left, max(maxPathVal, node.val)))
-            if node.right:
-                q.append((node.right, max(maxPathVal, node.val)))
-        return counter
+        self.counter = 0
+        def dfs(node, maxPathVal):
+            if not node: return
+            if node and node.val >= maxPathVal:
+                self.counter += 1
+                maxPathVal = node.val
+            dfs(node.left, maxPathVal)
+            dfs(node.right, maxPathVal)
+        dfs(root, root.val)
+        return self.counter
+    
+        # counter = 0
+        # q = collections.deque() # hold node : maxPathVal
+        # q.append((root, root.val))
+        # while q:
+        #     node, maxPathVal = q.popleft()
+        #     if node.val >= maxPathVal: counter += 1
+        #     if node.left:
+        #         q.append((node.left, max(maxPathVal, node.val)))
+        #     if node.right:
+        #         q.append((node.right, max(maxPathVal, node.val)))
+        # return counter
+    # T: O(n), S: O(w) - where w is the max width at a level within the tree
