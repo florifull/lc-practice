2842464d2c44dd2faddb8bdaaf613f38e091f700
florifull <93046441+florifull@users.noreply.github.com>
2025-09-13 17:56:08 -0400

    Time: 2 ms (38.73%) | Memory: 18.1 MB (34.43%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(localSol, choices):
+            if len(localSol) == len(nums):
+                res.append(localSol[:])
+                return
+            # decisions
+            for i in range(len(choices)):
+                localSol.append(choices[i])
+                dfs(localSol, choices[:i] + choices[i+1:])
+                localSol.pop()
+        dfs([], nums)
+        return res
