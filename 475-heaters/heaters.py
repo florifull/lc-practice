ba16e21d40547c38513de7e29b9f7a1a140ab29e
florifull <93046441+florifull@users.noreply.github.com>
2025-09-15 20:18:08 -0400

    Time: 165 ms (21.43%) | Memory: 22.6 MB (6.15%) - LeetSync

--- 475-heaters/heaters.py
+++ 475-heaters/heaters.py
@@ -1,21 +1,29 @@
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
-        houses.sort()
-        heaters.sort()
-        maxRadius = float('-inf')
+        houses_to_radius = {} # house : radius (abs(house - heater))
+        heaters.sort() # O(nlogn)
 
         for house in houses:
             l, r = 0, len(heaters) - 1
-            minRadius = float('inf')
-            # binary search
             while l <= r:
-                m = (l + r )// 2
-                heaterVal = heaters[m]
-                distance = abs(house - heaterVal)
-                if distance < minRadius: minRadius = distance
-
-                if heaterVal > house: r = m - 1
-                elif heaterVal < house: l = m + 1
-                else: break # found heater at exact position..
-            if minRadius > maxRadius: maxRadius = minRadius
-        return maxRadius
+                m = (l + r) // 2
+                heater = heaters[m]
+                distance = abs(house - heater)
+                if house in houses_to_radius:
+                    currMinDistance = houses_to_radius[house]
+                    if distance < currMinDistance:
+                        houses_to_radius[house] = distance
+                else:
+                    houses_to_radius[house] = distance
+                if house == heater:
+                    # exit while condition / binary search
+                    break
+                elif house < heater:
+                    r = m - 1
+                else:
+                    l = m + 1
+        minReqDistance = 0 # max of min required distances
+        for r in houses_to_radius.values():
+            if r > minReqDistance: minReqDistance = r
+        return minReqDistance
+    # T: O(hlogn) where h is houses, or O(nlogn) - S: O(h)
