c56422f97285306290d9cbffea88d415c5f0f3f0
florifull <93046441+florifull@users.noreply.github.com>
2025-09-21 11:07:53 -0400

    Time: 27 ms (94.01%) | Memory: 20.7 MB (11.44%) - LeetSync

--- 475-heaters/heaters.py
+++ 475-heaters/heaters.py
@@ -1,42 +1,14 @@
 class Solution:
-    # houses = [1, 2, 3]
-    # heaters =[-inf, 2, 2, 4, inf]
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         houses.sort()
         heaters.sort()
         heaters = [float('-inf')] + heaters + [float('inf')]
-        radius = 0
+        minRadius = float('-inf')
+        # houses = [1, 2, 3, 4]
+        # heaters = [1, 2, 3]
         r = 1
-        for h in houses:
-            while heaters[r] < h:
+        for _, house in enumerate(houses):
+            while house > heaters[r]:
                 r += 1
-            minRadius = min(h - heaters[r-1], heaters[r] - h)
-            radius = max(radius, minRadius)
-        return radius
-    #     houses_to_radius = {} # house : radius (abs(house - heater))
-    #     heaters.sort() # O(nlogn)
-
-    #     for house in houses:
-    #         l, r = 0, len(heaters) - 1
-    #         while l <= r:
-    #             m = (l + r) // 2
-    #             heater = heaters[m]
-    #             distance = abs(house - heater)
-    #             if house in houses_to_radius:
-    #                 currMinDistance = houses_to_radius[house]
-    #                 if distance < currMinDistance:
-    #                     houses_to_radius[house] = distance
-    #             else:
-    #                 houses_to_radius[house] = distance
-    #             if house == heater:
-    #                 # exit while condition / binary search
-    #                 break
-    #             elif house < heater:
-    #                 r = m - 1
-    #             else:
-    #                 l = m + 1
-    #     minReqDistance = 0 # max of min required distances
-    #     for r in houses_to_radius.values():
-    #         if r > minReqDistance: minReqDistance = r
-    #     return minReqDistance
-    # # T: O(hlogn) where h is houses, or O(nlogn) - S: O(h)
+            minRadius = max(minRadius, min(heaters[r] - house, house - heaters[r-1]))
+        return minRadius
