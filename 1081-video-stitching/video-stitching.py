ff16ad17e953feaa9bf612e17a4062597d15658d
florifull <93046441+florifull@users.noreply.github.com>
2025-09-21 14:33:07 -0400

    Time: 1 ms (18.39%) | Memory: 17.8 MB (67.26%) - LeetSync

--- 1081-video-stitching/video-stitching.py
+++ 1081-video-stitching/video-stitching.py
@@ -1,19 +1,14 @@
 class Solution:
     def videoStitching(self, clips: List[List[int]], time: int) -> int:
+        # clips = [[0,2],[1,9],[1,5],[4,6],[5,9],[8,10]] -> ex: prevStart=0
         clips.sort()
-        hops = 0
-        prevStart = newStart = 0
-        # [[0,2],[0, 1],[1,5],[1, 9],[4,6],[5,9],[8,10]]
-
-        # [[0,0],[0,6],[1,8],[2,3],[4,5],[5,7],[5,10],[7,10]]
-        i = 0
+        prevStart = count = i = 0
         while i < len(clips) and prevStart < time:
+            maxEnd = prevStart
             while i < len(clips) and clips[i][0] <= prevStart:
-                # tells us how far we can hop to for said start..
-                newStart = max(newStart, clips[i][1])
+                maxEnd = max(maxEnd, clips[i][1])
                 i += 1
-            if prevStart == newStart: return -1
-            prevStart = newStart
-            hops += 1
-        return hops if prevStart >= time else -1
-    # T: O(nlogn), S: O(1)
+            if prevStart == maxEnd: return -1
+            prevStart = maxEnd
+            count += 1
+        return count if prevStart >= time else -1
