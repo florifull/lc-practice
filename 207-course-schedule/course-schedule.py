75b80225f571f9dd3dbf07355f44b9727bcd3118
florifull <93046441+florifull@users.noreply.github.com>
2025-09-21 20:50:47 -0400

    Time: 7 ms (45.85%) | Memory: 19.2 MB (50.89%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereq = collections.defaultdict(list)
+        for c, pre in prerequisites:
+            prereq[c].append(pre)
+        # [[1,0],[0,1]] => {1->[0], 0->[1]}
+        visited = set()
+        def dfs(c):
+            if c not in prereq: return True
+            if c in visited: return False
+            visited.add(c)
+            for pre in prereq[c]:
+                if not dfs(pre): return False
+            visited.remove(c)
+            prereq[c] = []
+            return True
+        for i in range(numCourses):
+            if not dfs(i): return False
+        return True
