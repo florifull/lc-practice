1349c3e432dba21974de3c6ae651bfb5c45438a3
florifull <93046441+florifull@users.noreply.github.com>
2025-09-24 01:30:35 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (18.83%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,16 +1,39 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        l, r = -1, len(matrix[0])
+        t, b = -1, len(matrix)
         res = []
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        steps = [len(matrix[0]), len(matrix) - 1]
 
-        r, c, d = 0, -1, 0
-        while steps[d & 1]:
-            for i in range(steps[d & 1]):
-                r += directions[d][0]
-                c += directions[d][1]
-                res.append(matrix[r][c])
-            steps[d & 1] -= 1
-            d += 1
-            d %= 4
+        x = y = 0
+        while x in range(l, r) and y in range(t, b):
+            # move right
+            while y < r:
+                res.append(matrix[x][y])
+                y += 1
+            y -= 1
+            t += 1 # move our top border down one row
+            # move down
+            x += 1
+            while x < b:
+                res.append(matrix[x][y])
+                x += 1
+            x -= 1
+            r -= 1 # move our rightside border to the left
+            # move left one
+            y -= 1
+            # if we're all done moving around
+            if x <= t or x >= b or y <= l or y >= r: return res
+            while y > l:
+                res.append(matrix[x][y])
+                y -= 1
+            y += 1
+            b -= 1 # move bottom boundary up one
+            # move up one
+            x -= 1
+            while x > t:
+                res.append(matrix[x][y])
+                x -= 1
+            x += 1
+            l += 1 # move left boundary to the right once
+            y += 1 # shift to right and start over..
         return res
