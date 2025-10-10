c3d34d652d09f970977abf2e96aba65d29a2d085
florifull <93046441+florifull@users.noreply.github.com>
2025-10-10 16:20:16 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.12%) - LeetSync

--- 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
@@ -1,10 +1,6 @@
 class Solution:
     def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
         res = []
-        for i in range(len(nums)):
-            number = nums[i]
-            if len(res) >= index[i]+1:
-                res.insert(index[i], number)
-                continue
-            res.append(number)
+        for i, val in enumerate(index):
+            res.insert(index[i], nums[i])
         return res
