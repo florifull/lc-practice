ab745477d36dd7a1474589be0858a4ca2c5c3bed
florifull <93046441+florifull@users.noreply.github.com>
2025-10-16 16:25:41 -0400

    Time: 15 ms (50.34%) | Memory: 22.8 MB (11.68%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,10 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        occs_to_words = collections.defaultdict(list)
-        for s in strs:
-            chars = [0] * 26
-            for c in s:
-                chars[ord(c) - ord('a')] += 1
-            occs_to_words[tuple(chars)].append(s)
-        return [v for v in occs_to_words.values()]
-    # T: O(n * s), S: O(n)
+        uni_to_strings = collections.defaultdict(list)
+        # T: O(m * n)
+        for word in strs:
+            uni = [0] * 26
+            for c in word:
+                uni[ord(c) - ord('a')] += 1
+            uni_to_strings[tuple(uni)].append(word) # {(101...0):['helloo']}
+        return list(uni_to_strings.values())
