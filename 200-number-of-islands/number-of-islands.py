ce248316eb6b8218b5e4af2c1e0cd87d9f353bc0
florifull <93046441+florifull@users.noreply.github.com>
2025-11-01 12:08:58 -0400

    Time: 333 ms (5.26%) | Memory: 28.4 MB (10.08%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,21 +3,18 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         visited = set()
         islands = 0
-        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         def dfs(r, c):
+            if (r not in range(rows) or c not in range(cols)
+                or (r, c) in visited or grid[r][c] != '1'):
+                return
             visited.add((r, c))
-            # visit adjacent
-            for roff, coff in dirs:
-                if (r+roff in range(rows) and c+coff in range(cols) and 
-                    (r+roff, c+coff) not in visited and grid[r+roff][c+coff] == '1'):
-                    dfs(r+roff, c+coff)
-            return
+            for dr, dc in dirs:
+                dfs(r+dr, c+dc)
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in visited:
-                    # search path
+                if (r, c) not in visited and grid[r][c] == '1':
                     dfs(r, c)
                     islands += 1
         return islands
-    # T: O(R * C), S: O(R * C)
