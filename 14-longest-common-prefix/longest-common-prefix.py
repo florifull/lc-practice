6b41f84e54927ed9a09c07e7608425e1f2704ff3
florifull <93046441+florifull@users.noreply.github.com>
2025-11-01 13:18:58 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (30.07%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,37 +1,8 @@
-class Node:
-    def __init__(self):
-        self.children = {}
-        self.isTerminal = False
-
 class Solution:
-    def __init__(self):
-        self.root = Node()
-    
-    def add(self, s):
-        curr = self.root
-        for c in s:
-            if c not in curr.children:
-                newChild = Node()
-                curr.children[c] = newChild
-            curr = curr.children[c]
-        curr.isTerminal = True
-
-    def startsWith(self, s):
-        # if we get to a terminal node OR a node with more than 1 child - BREAK
-        # (no longer global common prefix..)
-        curr = self.root
-        prefix = []
-        for c in s:
-            if len(curr.children) > 1 or curr.isTerminal:
-                return ''.join(prefix)
-            prefix.append(c)
-            curr = curr.children[c]
-        return ''.join(prefix)
-
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs: return None
-        if len(strs) == 1: return strs[0]
-        # add all strings to trie
-        for s in strs:
-            self.add(s)
-        return self.startsWith(strs[0])
+        prefix = strs[0]
+        for pre in strs[1:]:
+            while not pre.startswith(prefix):
+                prefix = prefix[:len(prefix)-1]
+        return prefix
