8e34c594fafeb095d9023cc9d89bcd518a13a27b
florifull <93046441+florifull@users.noreply.github.com>
2025-11-01 13:01:24 -0400

    Time: 2 ms (30.26%) | Memory: 17.9 MB (30.07%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,10 +1,37 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.isTerminal = False
+
 class Solution:
+    def __init__(self):
+        self.root = Node()
+    
+    def add(self, s):
+        curr = self.root
+        for c in s:
+            if c not in curr.children:
+                newChild = Node()
+                curr.children[c] = newChild
+            curr = curr.children[c]
+        curr.isTerminal = True
+
+    def startsWith(self, s):
+        # if we get to a terminal node OR a node with more than 1 child - BREAK
+        # (no longer global common prefix..)
+        curr = self.root
+        prefix = []
+        for c in s:
+            if len(curr.children) > 1 or curr.isTerminal:
+                return ''.join(prefix)
+            prefix.append(c)
+            curr = curr.children[c]
+        return ''.join(prefix)
+
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        longest = []
-        for i in range(len(strs[0])):
-            char = strs[0][i]
-            for word in strs:
-                if i >= len(word) or word[i] != char: return ''.join(longest)
-            longest.append(char)
-        return ''.join(longest)
-    # T: O(n * m), S: O(m)
+        if not strs: return None
+        if len(strs) == 1: return strs[0]
+        # add all strings to trie
+        for s in strs:
+            self.add(s)
+        return self.startsWith(strs[0])
