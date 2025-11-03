adefecfbe244a6fa0b5ee1ed7e6f3c50650cf536
florifull <93046441+florifull@users.noreply.github.com>
2025-11-03 15:35:47 -0500

    Time: 163 ms (40.23%) | Memory: 63.5 MB (39.09%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.py
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -1,23 +1,20 @@
 class Solution:
     def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
         rows, cols = len(boxGrid), len(boxGrid[0])
-        # traverse boxGrid from right to left, and swap out elements accordingly
+        # traverse right to left, from top row down
         for r in range(rows):
-            e_pt = [r, cols-1]
-            for c in range(cols-1, -1, -1): # don't need to check leftmost (0th col.)
-                coordVal = boxGrid[r][c]
-                if coordVal == '.':
-                    continue
-                elif coordVal == '*':
-                    e_pt = [r, c-1]
-                else: # stone found
-                    boxGrid[e_pt[0]][e_pt[1]], boxGrid[r][c] = coordVal, boxGrid[e_pt[0]][e_pt[1]]
-                    e_pt = [e_pt[0], e_pt[1]-1]
-        # traverse from (bottom) left to right -> row, col -> col, row
-        newBox = [['' for r in range(rows)] for c in range(cols)]
-        # now I need to traverse new matrix and fill in corresponding rotated points
+            right = cols - 1
+            for c in range(cols-1, -1, -1):
+                # check for stationary obstacle
+                if boxGrid[r][c] == '*':
+                    right = c - 1
+                elif boxGrid[r][c] == '#':
+                    # swap right and left
+                    boxGrid[r][right], boxGrid[r][c] = boxGrid[r][c], boxGrid[r][right]
+                    right -= 1
+        # build new matrix
+        newMatrix = [['&' for r in range(rows)] for c in range(cols)]
         for r in range(rows-1, -1, -1):
             for c in range(cols):
-                newBox[c][rows - 1 - r] = boxGrid[r][c]
-        return newBox
-    # T: O(n * m), S: O(n * m)
+                newMatrix[c][-r-1] = boxGrid[r][c]
+        return newMatrix
