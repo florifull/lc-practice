bada65641664e5541e312b551c00e556e04a0b7a
florifull <93046441+florifull@users.noreply.github.com>
2025-11-04 21:48:38 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (66.38%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,17 +1,3 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        # 5, 5, 4, 3
-        # A, B, A, B -> A:9, B: 8
-        maxHeap = []
-        for p in piles:
-            heapq.heappush(maxHeap, -1 * p)
-        a = b = 0
-        turn = 2
-        while maxHeap:
-            absVal = abs(heapq.heappop(maxHeap))
-            if turn % 2 == 0:
-                a += absVal
-            else:
-                b += absVal
-            turn += 1
-        return a > b
+        return True
