2617f394d1120eddc6f9df9868fb20f469b0ea39
florifull <93046441+florifull@users.noreply.github.com>
2025-11-15 15:31:11 -0500

    Time: 46 ms (45.13%) | Memory: 22.5 MB (54.49%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,25 +1,17 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        # stack = [d, e, e] "edbbcccbdaa", k = 3 ... len(stack) >= k-1
-        stack = [s[0]]
+        # stack = [(char, count), ...]
+        # "deeedbbcccbdaa" -> [(d,1), (e,2)] ex: k = 3-1=2
+        stack = [[s[0], 1]]
         for i in range(1, len(s)):
-            # possibility of duplicate removal instance..
-            if len(stack) >= k-1:
-                window = stack[-1 * (k-1):]
-                duplicate = True
-                for w in window:
-                    if w != s[i]:
-                        stack.append(s[i])
-                        duplicate = False
-                        break
-                if not duplicate: continue
-                # found that we do have duplicates up to k-1 in window
-                j = 0
-                while j < k-1:
+            # if top of stack is equal to THIS char
+            if stack and stack[-1][0] == s[i]:
+                if stack[-1][1] >= k-1:
                     stack.pop()
-                    j += 1
-                continue
-            stack.append(s[i])
-        return ''.join(stack)
-                
+                    continue
+                # not at removal threshold
+                stack[-1][1] += 1
+            else:
+                stack.append([s[i], 1])
+        return ''.join(char * count for char, count in stack)
             
