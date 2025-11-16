ab39ccf8e7a44337a6069619f9bc35ae73fb9d10
florifull <93046441+florifull@users.noreply.github.com>
2025-11-16 14:29:55 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.05%) - LeetSync

---
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
@@ -0,0 +1,7 @@
+class Solution:
+    def equalFrequency(self, word: str) -> bool:
+        for i in range(len(word)):
+            new_w = word[:i] + word[i+1:]
+            new_w_map = collections.Counter(new_w)
+            if max(new_w_map.values()) == min(new_w_map.values()): return True
+        return False
