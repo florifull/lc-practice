570cc6ef63b7d6b8c8ec1557958ecb9ffd8e34aa
florifull <93046441+florifull@users.noreply.github.com>
2025-11-22 17:26:59 -0500

    Time: 3 ms (24.49%) | Memory: 17.9 MB (84.17%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -2,21 +2,15 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
-
-        def dfs(i, localSol):
-            if i >= len(nums):
-                res.append(localSol[:])
-                return
-            
-            # left
-            localSol.append(nums[i])
-            dfs(i+1, localSol)
-            # right
-            localSol.pop()
-            i += 1
-            while i < len(nums) and nums[i] == nums[i-1]:
-                i += 1
-            dfs(i, localSol)
+        def dfs(i, ans):
+            res.append(ans[:])
+            for j in range(i, len(nums)):
+                if j > i and nums[j] == nums[j-1]:
+                    continue
+                ans.append(nums[j])
+                dfs(j+1, ans)
+                ans.pop()
             return
         dfs(0, [])
         return res
+     # T: O(n * 2^n), S: O(n * 2^n)
