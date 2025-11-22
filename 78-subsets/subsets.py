b57951862cd3b1725df5cf9f210990607ba4cb60
florifull <93046441+florifull@users.noreply.github.com>
2025-11-22 15:43:00 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (39.48%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.sol = []
-
-        def dfs(i, arr):
-            if i > len(nums) - 1:
-                self.sol.append(arr[:])
-                return
-            arr.append(nums[i])
-            left = dfs(i+1, arr)
-            arr.pop()
-            right = dfs(i+1, arr)
+        res = []
+        seen = set()
+        def dfs(ans, arr):
+            res.append(ans[:])
+            # if tuple(ans[:]) not in seen:
+            #     res.append(ans[:])
+            #     seen.add(tuple(ans[:]))
+            for i in range(len(arr)):
+                ans.append(arr[i])
+                dfs(ans, arr[i+1:])
+                ans.pop()
             return
-            
-        dfs(0, [])
-        return self.sol
-    # T: O(n * 2^n), S: O(n)
+        dfs([], nums)
+        return res
