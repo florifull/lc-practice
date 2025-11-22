c7c66fe2dea3169d87ba0ce0580b40ff92754e39
florifull <93046441+florifull@users.noreply.github.com>
2025-11-22 16:49:53 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (21.01%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,12 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        def dfs(ans, arr):
+        def dfs(ans, i):
             res.append(ans[:])
-            for i in range(len(arr)):
-                ans.append(arr[i])
-                dfs(ans, arr[i+1:])
+            for j in range(i, len(nums)):
+                ans.append(nums[j])
+                dfs(ans, j+1)
                 ans.pop()
             return
-        dfs([], nums)
+        dfs([], 0)
         return res
+    # T: O(2^n * n), S: O(2^n * n)
