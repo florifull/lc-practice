361e0815111397df152994fce60af4533d85b441
florifull <93046441+florifull@users.noreply.github.com>
2025-11-22 16:31:09 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (21.01%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,12 +1,8 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        seen = set()
         def dfs(ans, arr):
             res.append(ans[:])
-            # if tuple(ans[:]) not in seen:
-            #     res.append(ans[:])
-            #     seen.add(tuple(ans[:]))
             for i in range(len(arr)):
                 ans.append(arr[i])
                 dfs(ans, arr[i+1:])
