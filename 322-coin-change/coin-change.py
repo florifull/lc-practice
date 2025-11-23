051ec20fdac74ac359f6be4da22f87d84c0ae895
florifull <93046441+florifull@users.noreply.github.com>
2025-11-23 16:42:44 -0500

    Time: 775 ms (51.53%) | Memory: 17.7 MB (99.71%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,14 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        dp = [0] * (amount+1)
-        # ex: [0, 1, 1, 2, 2, 1, 2, 2, 3, 3, 2, 3, 3] -> 
-        for i in range(1, amount+1):
-            # traverse coins backwards
-            coinAmount = float('inf')
-            for j in range(len(coins)-1, -1, -1):
-                if i >= coins[j]:
-                    leftover = i - coins[j]
-                    coinAmount = min(coinAmount, 1 + dp[leftover])
-            dp[i] = coinAmount
-        return dp[amount] if dp[amount] < (2**31) else -1
+        if not amount: return 0
+        dp = [float('inf')] * (amount+1)
+        dp[0] = 0
+        for i in range(1, len(dp)):
+            for c in coins:
+                subtract = i - c
+                if subtract < 0: subtract = i
+                dp[i] = min(dp[i], 1 + dp[subtract])
+        return dp[-1] if dp[-1] != float('inf') else -1
+        # [1, 4, 5] -> amount = 12 -> 
