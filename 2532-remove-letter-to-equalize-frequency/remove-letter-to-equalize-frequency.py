8ce18fc0feb34ccfb1b41d63e568bc4a0cb855ba
florifull <93046441+florifull@users.noreply.github.com>
2025-11-23 23:03:07 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (17.79%) - LeetSync

--- 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
@@ -9,9 +9,4 @@ class Solution:
             if max(ourMap.values()) == min(ourMap.values()): found = True
             ourMap[c] = ourMap.get(c, 0) + 1
         return found
-        # for i in range(len(word)):
-        #     new_w = word[:i] + word[i+1:]
-        #     new_w_map = collections.Counter(new_w)
-        #     if max(new_w_map.values()) == min(new_w_map.values()): return True
-        # return False
-    # T: O(n^2), S: O(n)
+    # T: O(n), S: O(26)
