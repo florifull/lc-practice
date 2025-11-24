31f875fa57f14046bf55d2b8638f4221c73a73bb
florifull <93046441+florifull@users.noreply.github.com>
2025-11-24 14:35:39 -0500

    Time: 3 ms (34.60%) | Memory: 19.9 MB (30.12%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,7 +6,6 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        
         def dfs(node, minV, maxV):
             if not node: return True
             if node.val <= minV or node.val >= maxV: return False
@@ -14,5 +13,4 @@ class Solution:
             right = dfs(node.right, node.val, maxV)
             return left and right
         return dfs(root, float('-inf'), float('inf'))
-        # node6: minV=5,maxV=float('inf)
-        # node3: minV=5,maxV=6
+    # T: O(n), S: O(n)
