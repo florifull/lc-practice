f92a3575fd5075dda3ad6f58af8412022dd8f519
florifull <93046441+florifull@users.noreply.github.com>
2025-11-23 23:02:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.69%) - LeetSync

--- 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
@@ -1,8 +1,17 @@
 class Solution:
     def equalFrequency(self, word: str) -> bool:
-        for i in range(len(word)):
-            new_w = word[:i] + word[i+1:]
-            new_w_map = collections.Counter(new_w)
-            if max(new_w_map.values()) == min(new_w_map.values()): return True
-        return False
+        ourMap = collections.Counter(word)
+        found = False
+        for c in list(ourMap.keys()):
+            ourMap[c] -= 1
+            if ourMap[c] == 0:
+                del ourMap[c]
+            if max(ourMap.values()) == min(ourMap.values()): found = True
+            ourMap[c] = ourMap.get(c, 0) + 1
+        return found
+        # for i in range(len(word)):
+        #     new_w = word[:i] + word[i+1:]
+        #     new_w_map = collections.Counter(new_w)
+        #     if max(new_w_map.values()) == min(new_w_map.values()): return True
+        # return False
     # T: O(n^2), S: O(n)
