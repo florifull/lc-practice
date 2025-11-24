d6a474ea3046afe11da03fff5552bde51ff77fbb
florifull <93046441+florifull@users.noreply.github.com>
2025-11-24 00:06:48 -0500

    Time: 4 ms (14.85%) | Memory: 21.1 MB (26.85%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,29 +6,12 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # iterative
-        node = root
-        s = []
-        curr = 0
-        while s or node: # just for first run time
-            while node:
-                s.append(node)
-                node = node.left
-            # leftmost of said node..
-            node = s.pop()
-            curr += 1
-            if curr == k: return node.val
-            # check right side
-            node = node.right
-
-        # inorder traversal (L, C, R)
-        # self.inorder = []
-        # def dfs(node):
-        #     if not node: return
-        #     left = dfs(node.left)
-        #     self.inorder.append(node.val)
-        #     right = dfs(node.right)
-        #     return
-        # dfs(root)
-        # return self.inorder[k-1]
-    # T: O(n), S: O(n)
+        res = []
+        def dfs(node):
+            if not node: return
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+            return
+        dfs(root)
+        return res[k-1]
