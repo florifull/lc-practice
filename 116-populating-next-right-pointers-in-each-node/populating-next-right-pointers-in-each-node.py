5d5c8b2d36f6b60b6f515ee8652856f19947bd79
florifull <93046441+florifull@users.noreply.github.com>
2025-11-24 00:02:41 -0500

    Time: 53 ms (44.86%) | Memory: 19.1 MB (50.85%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -1,25 +1,19 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
-
 class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root: return None
-        q = collections.deque()
-        q.append(root)
-        while q:
-            qSize = len(q)
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left: q.append(node.left)
-                if node.right: q.append(node.right)
-                if i < qSize - 1:
-                    # not at rightmost node
-                    node.next = q[0]
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node':
+        if not root:
+            return None
+        
+        if root.left:
+            root.left.next = root.right
+        
+        # 2. Connect the right child to the left child of the next parent
+        # This only happens if 'root.next' has already been set (i.e., root is not 
+        # the rightmost node of its level).
+        if root.right and root.next:
+            root.right.next = root.next.left
+        
+        # 3. Recurse down the tree
+        self.connect(root.left)
+        self.connect(root.right)
+        
         return root
