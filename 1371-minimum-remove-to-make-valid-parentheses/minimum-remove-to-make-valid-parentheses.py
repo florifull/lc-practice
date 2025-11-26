bc3a0074b788f798e6510b00d4cfdfe954006514
florifull <93046441+florifull@users.noreply.github.com>
2025-11-26 15:25:41 -0500

    Time: 376 ms (9.09%) | Memory: 19.7 MB (28.74%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,18 +1,21 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        to_remove = set()
-        stack = []
+        stack = [] # hold opening parentheses
+        closerRemoval = set() # hold closing parentheses that we NEED to remove..
 
         for i, c in enumerate(s):
             if c == '(':
                 stack.append(i)
             elif c == ')':
+                # there's no matching parentheses.. so mark it for REMOVAL
                 if not stack:
-                    to_remove.add(i)
-                    continue
-                stack.pop()
-        # note all open parentheses that we need to remove..
-        for i in stack:
-            to_remove.add(i)
-        
-        return ''.join(c for i, c in enumerate(s) if i not in to_remove)
+                    closerRemoval.add(i)
+                else:
+                    # we do have openers to match this closer
+                    stack.pop()
+        res = []
+        for i, c in enumerate(s):
+            if i in stack or i in closerRemoval:
+                continue
+            res.append(c)
+        return ''.join(res)
