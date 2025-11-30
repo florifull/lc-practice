77d9a1c1ad758505a1ce96648954f64cb94d13a0
florifull <93046441+florifull@users.noreply.github.com>
2025-11-30 16:37:48 -0500

    Time: 7 ms (22.55%) | Memory: 18.5 MB (92.10%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -7,16 +7,17 @@
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         # left = 2n, right = 2n+1 - width is right-left + 1 | ex:2: 14 - 8
+        # ex1: 7(right) - 4(left) + 1 = 4 (total width)
         ans = 1
         q = collections.deque()
         q.append((root,1))
 
         while q:
+            ans = max(ans, q[-1][1] - q[0][1] + 1)
             # check level
             for _ in range(len(q)):
                 node, level = q.popleft()
                 if node.left: q.append((node.left, 2 * level))
                 if node.right: q.append((node.right, 2 * level + 1))
-            if q:
-                ans = max(ans, q[-1][1] - q[0][1] + 1)
         return ans
+    # T: O(n), S: O(n)
